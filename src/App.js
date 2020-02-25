3d908d960ae59d7ba7161a9c2d5bf9cf2afedb2b
sonofab1rd <westernreserve@gmail.com>
2020-02-25 10:22:29 -0500

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component.jsx';
+import HomePage from "./pages/homepage/homepage.component.jsx";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-     <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
