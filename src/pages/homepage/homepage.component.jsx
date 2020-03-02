5ccbbd4f1e4571173017e7b42b6862732f44fb60
sonofab1rd <westernreserve@gmail.com>
2020-03-02 08:35:52 -0500

    added form-input and custom button

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import MenuItem from  '../../components/menu-item/menu-item.compenent';
 
 const HomePage = () => (
     <div className = 'homepage'>
